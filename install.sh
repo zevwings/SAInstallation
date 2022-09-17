c5acfec240e10795c3b8ba3e6ed4c6fc430374fe
zevwings <zevwings@foxmail.com>
2022-09-17 21:11:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ else
 echo "文件夹已经存在"
 fi
 
-echo "开始写入 Fastfile ..."
+echo "开始写入Fastfile..."
 
 cat > "${fastlane_dir}/Fastfile" <<EOF
 default_platform(:ios)
@@ -35,7 +35,7 @@ end
 
 EOF
 
-echo "写入 Fastfile 成功"
+echo "写入Fastfile成功"
 
 echo "开始执行 fastlane 命令 ..."
 fastlane install project_name:"$PROJECT_NAME" git_ssh_url:"$GIT_SSH_URL" git_homepage_url:"$GIT_HOMEPAGE_URL"
